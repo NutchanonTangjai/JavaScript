946167c4e15d03bfc83a8a55b50cae23507fd40e
NutchanonTangjai <6206021611109@fitm.kmutnb.ac.th>
2021-03-25 00:42:04 +0700

    update

---
+++ 10 unique edabit/calculateScore.js
@@ -0,0 +1,12 @@
+function calculateScore(arr){
+    for(i=0;i<arr.length;i++){
+        if(arr[i][0] == "R"){
+            if(arr[i][1] == ""){
+            
+            }
+        }
+    }
+}
+calculateScore([["R", "P"], ["R", "S"], ["S", "P"]])
+calculateScore([["R", "R"], ["S", "S"]])
+calculateScore([["S", "R"], ["R", "S"], ["R", "R"]])
