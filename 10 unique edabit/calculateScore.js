3c923d494c7e3bc6e89db6d9bf58d86d74d6219f
NutchanonTangjai <6206021611109@fitm.kmutnb.ac.th>
2021-03-25 09:27:46 +0700

    update

--- 10 unique edabit/calculateScore.js
+++ 10 unique edabit/calculateScore.js
@@ -1,12 +1,63 @@
-function calculateScore(arr){
-    for(i=0;i<arr.length;i++){
-        if(arr[i][0] == "R"){
-            if(arr[i][1] == ""){
-            
-            }
-        }
+function calculateScore(arr) {
+  let check = 0;
+  for (i = 0; i < arr.length; i++) {
+    p1 = arr[i][0];
+    p2 = arr[i][1];
+    if (p1 == "R") {
+      if (p2 == "R") {
+        return "Tie";
+      }
+      if (p2 == "P") {
+        check--;
+      }
+      if (p2 == "S") {
+        check++;
+      }
+    } else if (p1 == "P") {
+      if (p2 == "R") {
+        check++;
+      }
+      if (p2 == "P") {
+        return "Tie";
+      }
+      if (p2 == "S") {
+        check--;
+      }
+    } else if (p1 == "S") {
+      if (p2 == "R") {
+        check--;
+      }
+      if (p2 == "P") {
+        check++;
+      }
+      if (p2 == "S") {
+        return "Tie";
+      }
     }
+  }
+  if (check > 0) {
+    return "Abigail";
+  } else {
+    return "Benson";
+  }
 }
-calculateScore([["R", "P"], ["R", "S"], ["S", "P"]])
-calculateScore([["R", "R"], ["S", "S"]])
-calculateScore([["S", "R"], ["R", "S"], ["R", "R"]])
+console.log(
+  calculateScore([
+    ["R", "P"],
+    ["R", "S"],
+    ["S", "P"],
+  ])
+);
+console.log(
+  calculateScore([
+    ["R", "R"],
+    ["S", "S"],
+  ])
+);
+console.log(
+  calculateScore([
+    ["S", "R"],
+    ["R", "S"],
+    ["R", "R"],
+  ])
+);
